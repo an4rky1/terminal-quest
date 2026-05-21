7602f2b81585a05c276fa3f376d2d19b8e4961c1
anarky <anarky13@proton.me>
2026-05-21 20:39:29 +0300

    fix: correct migration order and nginx envsubst
    Rename items migration to run before room_connections.
    Use plain ${PORT} in nginx.conf with default in start.sh.

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PORT="${PORT:-8080}"
+
 envsubst '${PORT}' < /etc/nginx/sites-enabled/default > /tmp/nginx.conf && mv /tmp/nginx.conf /etc/nginx/sites-enabled/default
 
 php artisan migrate --force
